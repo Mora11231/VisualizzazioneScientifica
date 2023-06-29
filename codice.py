45a7de53f65b82a1581a9f0d158f8607eb9a6d2c
Marco Morandi <marco.morandi3@studenti.unimi.it>
2023-06-29 12:47:41 +0200

    Aggiunto SunBurst per covid, aggiunto diagramma a barre F2PvsP2P numero di giochi usciti all'anno

--- codice.py
+++ codice.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import plotly.express as px
 import plotly.graph_objs as go
 import re
+from datetime import date
 
 def checkPerData(x):
     if len(x) == 8:
@@ -237,4 +238,95 @@ def EstrazioneGeneri():
     #df.to_csv('fileAggiornatoGeneri.csv',index=False)
 
     print(df['Indie'].corr(df['Casual']))
-EstrazioneGeneri()
+#EstrazioneGeneri()
+
+
+def checkMultiplayer(x):
+    return bool(re.search(r'\b(?:\w+,\s)?Multi-player(?:,\s\w+)?\b', x))
+
+def checkSingleplayer(x):
+    return bool(re.search(r'\b(?:\w+,\s)?Single-player(?:,\s\w+)?\b', x))
+
+def checkCoop(x):
+    return bool(re.search(r'\b(?:\w+,\s)?Co-op(?:,\s\w+)?\b', x))
+
+def checkMultiSingle(x):
+    return (checkMultiplayer(x) & checkSingleplayer(x))
+
+def sunBurstPerCovid():
+    df = pd.read_csv('fileAggiornatoF2P.csv')
+    df = df[['Name','Release date','Categories']]
+
+    df['Release date'] = df['Release date'].astype('datetime64[ns]')
+    df['Categories'] = df['Categories'].apply(lambda x: replaceNaN(x))
+
+    df['Multi'] = df['Categories'].apply(lambda x: checkMultiplayer(x))
+    df['Single'] = df['Categories'].apply(lambda x: checkSingleplayer(x))
+    df['Coop'] = df['Categories'].apply(lambda x: checkCoop(x))
+    df['MultiSingle'] = df['Categories'].apply(lambda x: checkMultiSingle(x))
+
+
+    pre = date(2020,1,1)
+    post = date(2022,1,1)
+
+    pre_covid = df[df['Release date'].dt.date < pre]
+    covid = df[(df['Release date'].dt.date >= pre) & (df['Release date'].dt.date < post) ]
+    post_covid = df[df['Release date'].dt.date >= post]
+
+    data = dict(
+        periodo=['Pre-Covid','Pre-Covid','Pre-Covid','Pre-Covid','DuranteCovid','DuranteCovid','DuranteCovid','DuranteCovid', 'Covid','Covid','Covid','Covid'],
+        generi=['SiglePlayer','Multiplayer','Coop','MultiSingle','SiglePlayer','Multiplayer','Coop','MultiSingle','SiglePlayer','Multiplayer','Coop','MultiSingle'],
+        value = [sum(pre_covid.Single),sum(pre_covid.Multi),sum(pre_covid.Coop),sum(pre_covid.MultiSingle),sum(covid.Single),sum(covid.Multi),sum(covid.Coop),sum(covid.MultiSingle),sum(post_covid.Single),sum(post_covid.Multi),sum(post_covid.Coop),sum(post_covid.MultiSingle)]
+    )
+
+    fig = px.sunburst(
+    data,
+    path=['periodo','generi'],
+    values='value',
+    )
+    fig.show()
+#sunBurstPerCovid()
+
+def replaceNanNum(x):
+    if x != x:
+        return 0
+    return x
+def diagrammaBarreF2PvsP2P():
+    df = pd.read_csv('fileAggiornatoF2P.csv')
+    df['Release date'] = df['Release date'].astype('datetime64[ns]')
+
+    df['year'] = df['Release date'].apply(lambda x: x.year)
+
+    P2P = df[df['F2P'] == False]
+    F2P = df[df['F2P'] == True]
+
+
+
+    series_P2P = P2P[['year']].value_counts().sort_index()
+    series_F2P = F2P[['year']].value_counts().sort_index()
+
+    series_F2P.apply(lambda x : replaceNanNum(x))
+    series_P2P.apply(lambda x : replaceNanNum(x))
+
+
+
+    sum_p2p = series_P2P.cumsum().sort_index()
+    series_P2P[2008] = sum_p2p[2008]
+    series_P2P = series_P2P[11:-1]
+
+    sum_f2p = series_F2P.cumsum().sort_index()
+    series_F2P[2008] = sum_f2p[2008]
+    series_F2P = series_F2P.loc[2008:]
+    
+
+    df = pd.DataFrame(columns = ['P2P', 'F2P'])
+    df['P2P'] = series_P2P
+    df['F2P'] = series_F2P
+
+    df.reset_index(inplace=True)
+    fig = px.bar(df, x='year', y=['P2P','F2P'],
+            barmode='group',
+            )
+    fig.show()
+
+diagrammaBarreF2PvsP2P()
